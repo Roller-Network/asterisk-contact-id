f05fa5f289966b0e8f45aabcc29b4b19ea4970ca
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2023-07-30 13:45:44 -0700

    Update asteriskcontactid.sql

--- schema/asteriskcontactid.sql
+++ schema/asteriskcontactid.sql
@@ -4,16 +4,16 @@
 
 DROP TABLE IF EXISTS `alarmreceiver`;
 CREATE TABLE `alarmreceiver` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `account` smallint(5) unsigned NOT NULL,
-  `event` varchar(16) NOT NULL,
-  `protocol` enum('ADEMCO_CONTACT_ID') NOT NULL,
-  `callingfrom` varchar(80) NOT NULL DEFAULT '',
-  `callername` varchar(80) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
-  KEY `account` (`account`),
-  KEY `callingfrom` (`callingfrom`)
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    `account` smallint(5) unsigned NOT NULL,
+    `event` varchar(16) NOT NULL,
+    `protocol` enum('ADEMCO_CONTACT_ID') NOT NULL,
+    `callingfrom` varchar(80) NOT NULL DEFAULT '',
+    `callername` varchar(80) NOT NULL DEFAULT '',
+    PRIMARY KEY (`id`),
+    KEY `account` (`account`),
+    KEY `callingfrom` (`callingfrom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -23,8 +23,8 @@ CREATE TABLE `alarmreceiver` (
 DROP TABLE IF EXISTS `alarmreceiver_test`;
 
 CREATE TABLE `alarmreceiver_test` (
-  `account` smallint(5) unsigned NOT NULL,
-  `test_interval` smallint(5) unsigned NOT NULL DEFAULT '24',
-  `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  PRIMARY KEY (`account`)
+    `account` smallint(5) unsigned NOT NULL,
+    `test_interval` smallint(5) unsigned NOT NULL DEFAULT '24',
+    `timestamp` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
