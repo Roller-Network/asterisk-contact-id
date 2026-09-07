ad04242e87101afe76a06b7a977caa0a2f2fad39
WillCodeForCats <48533968+WillCodeForCats@users.noreply.github.com>
2026-09-07 07:49:27 -0700

    Fix dbiUser typo

--- asteriskcontactid.pl
+++ asteriskcontactid.pl
@@ -21,7 +21,7 @@ use MIME::Lite;
 
 my $spoolDir = '/var/spool/asterisk/alarmreceiver';
 my $dbi = "DBI:mysql:host=localhost;database=asterisk";
-my $dbuIser = "asterisk";
+my $dbiUser = "asterisk";
 my $dbiPassword = "";
 
 my $emailFrom = 'alarmreceiver@example.com';
